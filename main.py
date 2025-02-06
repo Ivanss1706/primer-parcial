543c2238d36180007953ba29378514ddab41374f
Ivan Solano <77413944+Ivanss1706@users.noreply.github.com>
2025-02-06 12:21:11 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#IMPORTACION DE FASTAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+#IMPORTAMCION DESDE LA CARPETA ROUTER
+from Routers import country, continent, regions, antartica
+
+#CREACION DEL OBJETO APP
+app = FastAPI()
+app.include_router(country.router)
+app.include_router(continent.router)
+app.include_router(regions.router)
+app.include_router(antartica.router)
+
+@app.get("/")
+async def imprimir():
+    return "Hola todos"
+
+
